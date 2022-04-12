708056c4f3ef9b61763491e7f0285fe75be2be77
Alejo9601 <alejandro96jm@gmail.com>
2022-04-12 17:56:27 -0300

    Changed logo from navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,4 @@
-import logo from "../assets/icons/logo.png";
+import logo from "../assets/icons/LogoCimacode.png";
 import "../styles/navbar.css";
 import MobileMenu from "./MobileMenu";
 import DesktopMenu from "./DesktopMenu";
