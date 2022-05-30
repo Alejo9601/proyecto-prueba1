3c8a4dd31db176e4b553003b62f7beb6b6281568
Alejo9601 <alejandro96jm@gmail.com>
2022-05-30 09:38:52 -0300

    Aadd

--- src/components/RefButton.js
+++ src/components/RefButton.js
@@ -1,18 +1,16 @@
 const RefButton = ({ className, reference, btnText }) => {
   return (
-    <>
-      <div className="flex-container refButton">
-        <a
-          style={{ textDecoration: "none" }}
-          href={reference}
-          className={className}
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          {btnText}
-        </a>
-      </div>
-    </>
+    <div className="flex-container refButton">
+      <a
+        style={{ textDecoration: "none" }}
+        href={reference}
+        className={`${className} ${reference !== "" ? "" : "btn-disabled"}`}
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        {btnText}
+      </a>
+    </div>
   );
 };
 
