1937d14f422f2efa1723115283d272c41e2e8c8c
Alejo9601 <alejandro96jm@gmail.com>
2022-07-20 15:43:02 -0300

    Deleted useless code

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -1,10 +1,6 @@
 import { useContext, useEffect, useRef } from "react";
 import "../styles/contact.css";
 import contactMeans from "../assets/json/contact_means.json";
-// import gmail from "../assets/icons/gmail.png";
-// import github from "../assets/icons/github.png";
-// import twitter from "../assets/icons/twitter.png";
-// import linkedin from "../assets/icons/linkedin.png";
 import SectionContext from "../context/SectionContext";
 import ContactItem from "../components/ContactItem";
 
@@ -34,45 +30,6 @@ const Contact = () => {
           />
         );
       })}
-      {/* <div className="flex-container contact-item" id="">
-        <a
-          href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSPGFjDGBffFvZLjSNnFgScgtNRFHgRpnfxPCXqGNjBzMPThptSbJtzLSCQpPSXpwStVPzdH"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img className="img-64px" src={gmail} alt="" />
-        </a>
-        <h3 className="img-description  contact-label">Mail Me</h3>
-      </div>
-
-      <div className="flex-container contact-item" id="">
-        <a href="https://github.com/Alejo9601" target="_blank" rel="noreferrer">
-          <img className="img-64px" src={github} alt="" />
-        </a>
-        <h3 className="img-description  contact-label">Follow Me</h3>
-      </div>
-
-      <div className="flex-container contact-item" id="">
-        <a
-          href="https://twitter.com/AlejandroJ1896"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img className="img-640px" src={twitter} alt="" />
-        </a>
-        <h3 className="img-description  contact-label">Follow Me</h3>
-      </div>
-
-      <div className="flex-container contact-item" id="">
-        <a
-          href="https://www.linkedin.com/in/alejandro-juarez96"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img className="img-640px" src={linkedin} alt="" />
-        </a>
-        <h3 className="img-description contact-label">Contact Me</h3>
-      </div> */}
     </div>
   );
 };
